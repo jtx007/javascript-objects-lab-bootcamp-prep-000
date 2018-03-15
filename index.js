b18b165623ff7a2e240af3ba2cd6998802848e4d
James Thomas <jamesjacobthomas7@gmail.com>
2018-03-15 00:13:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {
 
 }
 
-function updatesObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
